6bad0c26a1ac0d02ebf5e1d895291d749c02acae
mtutarinov <mihailtutarinov2@gmail.com>
2024-08-29 11:38:55 +0300

    add nginx and gunicorn

--- project/referral_system/urls.py
+++ project/referral_system/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include, re_path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls', namespace='core')),
     re_path(r'^auth/', include('drf_social_oauth2.urls', namespace='drf')),
 ]
+
